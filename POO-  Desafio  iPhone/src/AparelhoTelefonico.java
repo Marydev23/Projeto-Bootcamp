43a9915b8719eaa3ca924bbbe1bc11e97818c967
Marydev23 <marilzasantos152@gmail.com>
2024-06-26 21:48:44 -0300

    comit

---
+++ POO-  Desafio  iPhone/src/AparelhoTelefonico.java
@@ -0,0 +1,9 @@
+public class AparelhoTelefonico {
+    void ligar(String numero);
+    void atender();
+    void iniciarCorreioVoz();
+    
+}
+
+    
+
---
+++ POO-  Desafio  iPhone/src/NavegadorInternet.java
@@ -0,0 +1,6 @@
+public class NavegadorInternet {
+    void exibirPagina(Strinf url);
+    void AdicionarNovaAba();
+    void atualizar();
+    
+}
